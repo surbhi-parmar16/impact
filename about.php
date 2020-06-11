8b49b3b93e3cdce527edc91ec0ff5c96bae8a894
surbhi-parmar16 <66787173+surbhi-parmar16@users.noreply.github.com>
2020-06-11 21:07:35 +0530

    Finish all
    code and images

---
+++ about.php
@@ -0,0 +1,432 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+    <title>Impact</title>
+
+
+
+	<meta name="theme-color" content="#ff0000">
+
+	<!-- Windows Phone -->
+
+	<meta name="msapplication-navbutton-color" content="#4285f4">
+
+	<!-- iOS Safari -->
+
+	<meta name="apple-mobile-web-app-status-bar-style" content="#4285f4">
+
+	<link rel="icon" sizes="192x192" href="img/logo.jpeg">
+
+
+
+    <!-- Bootstrap Core CSS -->
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+    <!-- Main stylesheet -->
+
+    <link href="assets/css/social.css" rel="stylesheet">
+     <link href="css/css.css" rel="stylesheet">
+
+
+
+    <!-- Color stylesheet -->
+
+    <!-- <link href="assets/css/colors.css" rel="stylesheet"> -->
+
+    <!-- <link href="assets/css/colors_2.css" rel="stylesheet"> -->
+
+    <!-- <link href="assets/css/colors_3.css" rel="stylesheet"> -->
+
+    <!-- <link href="assets/css/colors_4.css" rel="stylesheet"> -->
+
+
+
+
+
+    <!-- Plugin stylesheets -->
+
+    <link href="assets/css/plugins/owl.carousel.css" rel="stylesheet">
+
+    <link href="assets/css/plugins/owl.theme.css" rel="stylesheet">
+
+    <link href="assets/css/plugins/owl.transitions.css" rel="stylesheet">
+
+    <link href="assets/css/plugins/animate.css" rel="stylesheet">
+
+    <link href="assets/css/plugins/magnific-popup.css" rel="stylesheet">
+
+    <link href="assets/css/plugins/jquery.mb.YTPlayer.min.css" rel="stylesheet">
+
+	 <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
+
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+
+
+    <!-- Custom Fonts -->
+
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <link href="http://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css">
+
+
+
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:446112,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
+
+</head>
+
+
+
+<body id="home">
+
+            <!-- Navigation -->
+
+            <nav class="navbar navbar-custom navbar-fixed-top" role="navigation" style="height:85px">
+
+                <div class="container">
+
+                    <div class="navbar-header pull-left">
+
+                        <a class="navbar-brand page-scroll" href="#page-top">
+
+                            <p class="brand-logo"><img src="img/logo.jpeg" style="width:75px;height:75px;color:white;" class="img-responsive"></p>
+
+
+
+                        </a>
+
+                    </div>
+
+                    <div class="main-nav pull-right">
+
+                        <div class="button_container toggle">
+
+                            <span class="top"></span>
+
+                            <span class="middle"></span>
+
+                            <span class="bottom"></span>
+
+                        </div>
+
+                    </div>
+
+                    <div class="overlay" id="overlay">
+
+                        <nav class="overlay-menu">
+
+                            <ul>
+
+								<li><a href="index.html">Home</a></li>
+
+                                <li><a href="about.php">About Us</a></li>
+
+                                <li><a href="learning.php">Learning Programs</a></li>
+
+                                <li><a href="events.php">Events</a></li>
+
+                                <li><a href="founders.php">Founders</a></li>
+
+                                <li><a href="#acheivements">Our Acheivements</a></li>
+
+                                <li><a href="#contact">Contact us</a></li>
+
+                            </ul>
+
+                        </nav>
+
+                    </div>
+
+                </div><!-- /.container -->
+
+            </nav>
+
+
+
+
+            <!-- Full Page Image Background Carousel Header -->
+
+            <header id="intro-carousel" class="carousel slide">
+
+                <!-- Optional Indicators -->
+
+                <!-- <ol class="carousel-indicators">
+
+                        <li data-target="#intro-carousel" data-slide-to="0" class="active"></li>
+
+                        <li data-target="#intro-carousel" data-slide-to="1"></li>
+
+                        <li data-target="#intro-carousel" data-slide-to="2"></li>
+
+                    </ol> -->
+
+                <!-- Wrapper for slides -->
+
+               <!--  <div class="carousel-inner"> -->
+
+                    <!-- <div class="item active"> -->
+
+                        <!-- Set the first background image using inline CSS below. -->
+
+                        <div class="fill" style="background-image:url('img/about.jpg');"></div>
+
+                        <div class="carousel-caption">
+
+                            <!-- <h1 class="wow animated slideInDown">Impact</h1> -->
+                            
+                             <h1 class="wow animated slideInDown" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><strong><br><br>About Us <br></h1>
+
+                            <p>One should always make an, ‘impact,’ with their words, and we provide the path and platform for it. 
+From providing sessions on improving communication and public speaking skills to having fully comprehensive training sessions for MUNs, we build one’s confidence and leadership skills.
+</p>
+
+                            <p>We not only encourage one to express their opinions and viewpoints, bringing in a diversity of perspectives, but we also provide a platform for inspiring and innovative souls to make their voices heard. Having riveting speakers enlighten our registrants, we connect the world of tomorrow’s leaders with our nourishment of curbing stage fright, being motivated, and building self-confidence. </p>
+                            
+
+                            <!-- <span><a href="organization.html" class="btn btn-default btn-lg" target="_blank">ORGANIZATION</a>
+
+                             <a href="volunteer.html" class="btn btn-default btn-lg" target="_blank" style="margin:5px;"> VOLUNTEER</a></span> -->
+
+                        </div>
+
+                        <div class="overlay-detail"></div>
+
+                    <!-- </div> --><!-- /.item -->
+
+
+               <!--  </div> -->
+
+                <!-- Controls -->
+
+                <a class="left carousel-control squared" href="#intro-carousel" data-slide="prev">
+
+                    <i class="fa fa-angle-left fa-2x"></i>
+
+                </a>
+
+                <a class="right carousel-control squared" href="#intro-carousel" data-slide="next">
+
+                    <i class="fa fa-angle-right fa-2x"></i>
+
+                </a>
+
+                <div class="mouse"></div>
+
+            </header>
+
+    
+
+    <section class="ftco-section services-section bg-light">
+      <div class="container">
+        <div class="row d-flex">
+          <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block text-center">
+              <div class="d-flex justify-content-center"><div class="icon"><span class="flaticon-guarantee"></span></div></div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3"><br>Our Mission</h3>
+                <p>Our primary mission is to shape the registrants as compelling speakers, great leaders, and innovative change makers. Additionally, we want to create a safe space for people to voice their perspectives and opinions without being judged as that is the foundation of a confident speaker. </p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block text-center">
+              <div class="d-flex justify-content-center"><div class="icon"><span class="flaticon-like"></span></div></div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3"><br>Our Vision</h3>
+                <p>Our primary mission is to shape the registrants as compelling speakers, great leaders, and innovative change makers. Additionally, we want to create a safe space for peop</p>
+              </div>
+            </div>    
+          </div>
+          <!-- <div class="col-md-3 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block text-center">
+              <div class="d-flex justify-content-center"><div class="icon"><span class="flaticon-detective"></span></div></div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3">Best Travel Agent</h3>
+                <p>A small river named Duden flows by their place and supplies.</p>
+              </div>
+            </div>      
+          </div> -->
+          <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+            <div class="media block-6 services d-block text-center">
+              <div class="d-flex justify-content-center"><div class="icon"><span class="flaticon-support"></span></div></div>
+              <div class="media-body p-2 mt-2">
+                <h3 class="heading mb-3"><br>Our Goal</h3>
+                <p></p>
+              </div>
+            </div>      
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+
+
+
+            <!-- Footer -->
+
+            <footer>
+
+                <div class="container">
+
+                    <div class="row text-center">
+
+                        <div class="col-md-12 segment">
+
+                            <a href="#" title="Hallooou - HTML5 Template">
+
+                                <h2>
+
+                                    Impact
+
+                                </h2>
+
+                            </a>
+
+                            <p class="white">In a world like today, your voice needs to be heard and reverberated till it leaves a mark of change.</p>
+
+                        </div><!-- /.col-md-12 -->
+
+                    </div><!-- /.row -->
+
+
+
+                    <div class="row text-center">
+
+                        <div class="col-md-12 social segment">
+
+                            <h4>Stay Connected</h4>
+
+                            <a href="#"><i class="fa fa-facebook fa-3x"></i></a>
+
+                            <a href="#"><i class="fa fa-twitter fa-3x"></i></a>
+
+                            <a href="#"><i class="fa fa-linkedin fa-3x"></i></a>
+
+                        </div><!-- /.col-md-12 -->
+
+                    </div><!-- /.row -->
+
+
+
+                </div><!-- /.container -->
+
+
+
+                <div class="copynote">
+
+                    <div class="container">
+
+                        <div class="row">
+
+                            <div class="col-md-12 text-center">
+
+                                &copy; 2020. Impact. All rights reserved.
+
+                            </div><!-- /.col-md-12 -->
+
+
+
+                        </div><!-- /.row -->
+
+                    </div><!-- /.container -->
+
+                </div><!-- /.copynote -->
+
+
+
+                <div class="nav pull-right scroll-top">
+
+                    <a href="#home" title="Scroll to top"><i class="fa fa-angle-up"></i></a>
+
+                </div>
+
+
+
+            </footer>
+
+
+
+
+
+    <!-- jQuery -->
+
+    <script src="assets/js/jquery.min.js"></script>
+
+
+
+    <!-- Bootstrap Core JavaScript -->
+
+    <script src="assets/js/bootstrap.min.js"></script>
+
+
+
+    <!-- Plugin JavaScript -->
+
+    <script src="assets/js/plugins/wow.min.js"></script>
+
+    <script src="assets/js/plugins/owl.carousel.min.js"></script>
+
+    <script src="assets/js/plugins/jquery.parallax-1.1.3.js"></script>
+
+    <script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+
+    <script src="assets/js/plugins/jquery.mb.YTPlayer.min.js"></script>
+
+    <script src="assets/js/plugins/jquery.countTo.js"></script>
+
+    <script src="assets/js/plugins/jquery.inview.min.js"></script>
+
+    <script src="assets/js/plugins/pace.min.js"></script>
+
+    <script src="assets/js/plugins/jquery.easing.min.js"></script>
+
+    <script src="assets/js/plugins/jquery.validate.min.js"></script>
+
+    <script src="assets/js/plugins/additional-methods.min.js"></script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBoVKfEihX__NdMwdDysA6Vve6PE9Ierj4"></script>
+
+
+
+    <!-- Custom JavaScript -->
+
+    <script src="assets/js/hallooou.js"></script>
+
+</body>
+
+
+
+</html>
+
