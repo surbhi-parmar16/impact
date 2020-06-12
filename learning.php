51a885eeaf6dfb053b3d2df27db6eca6755cd864
surbhi-parmar16 <66787173+surbhi-parmar16@users.noreply.github.com>
2020-06-12 18:24:30 +0530

    Add files via upload

---
+++ learning.php
@@ -0,0 +1,478 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+    <title>Impact</title>
+
+
+
+	<meta name="theme-color" content="#ff0000">
+
+	<!-- Windows Phone -->
+
+	<meta name="msapplication-navbutton-color" content="#4285f4">
+
+	<!-- iOS Safari -->
+
+	<meta name="apple-mobile-web-app-status-bar-style" content="#4285f4">
+
+	<link rel="icon" sizes="192x192" href="img/logo.jpeg">
+
+
+
+    <!-- Bootstrap Core CSS -->
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+    <!-- Main stylesheet -->
+
+    <link href="assets/css/social.css" rel="stylesheet">
+     <link href="css/css.css" rel="stylesheet">
+
+
+
+    <!-- Color stylesheet -->
+
+    <!-- <link href="assets/css/colors.css" rel="stylesheet"> -->
+
+    <!-- <link href="assets/css/colors_2.css" rel="stylesheet"> -->
+
+    <!-- <link href="assets/css/colors_3.css" rel="stylesheet"> -->
+
+    <!-- <link href="assets/css/colors_4.css" rel="stylesheet"> -->
+
+
+
+
+
+    <!-- Plugin stylesheets -->
+
+    <link href="assets/css/plugins/owl.carousel.css" rel="stylesheet">
+
+    <link href="assets/css/plugins/owl.theme.css" rel="stylesheet">
+
+    <link href="assets/css/plugins/owl.transitions.css" rel="stylesheet">
+
+    <link href="assets/css/plugins/animate.css" rel="stylesheet">
+
+    <link href="assets/css/plugins/magnific-popup.css" rel="stylesheet">
+
+    <link href="assets/css/plugins/jquery.mb.YTPlayer.min.css" rel="stylesheet">
+
+	 <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
+
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+
+
+    <!-- Custom Fonts -->
+
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <link href="http://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css">
+
+
+
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:446112,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
+
+</head>
+
+
+
+<body id="home">
+
+            <!-- Navigation -->
+
+            <nav class="navbar navbar-custom navbar-fixed-top" role="navigation" style="height:85px">
+
+                <div class="container">
+
+                    <div class="navbar-header pull-left">
+
+                        <a class="navbar-brand page-scroll" href="#page-top">
+
+                            <p class="brand-logo"><img src="img/logo.jpeg" style="width:75px;height:75px;color:white;" class="img-responsive"></p>
+
+
+
+                        </a>
+
+                    </div>
+
+                    <div class="main-nav pull-right">
+
+                        <div class="button_container toggle">
+
+                            <span class="top"></span>
+
+                            <span class="middle"></span>
+
+                            <span class="bottom"></span>
+
+                        </div>
+
+                    </div>
+
+                    <div class="overlay" id="overlay">
+
+                        <nav class="overlay-menu">
+
+                            <ul>
+
+								<li><a href="index.html">Home</a></li>
+
+                                <li><a href="about.php">About Us</a></li>
+
+                                <li><a href="learning.php">Learning Programs</a></li>
+
+                                <li><a href="events.php">Events</a></li>
+
+                                <li><a href="founders.php">Founders</a></li>
+
+								<li><a href="#acheivements">Our Acheivements</a></li>
+
+                                <li><a href="#contact">Contact us</a></li>
+
+                            </ul>
+
+                        </nav>
+
+                    </div>
+
+                </div><!-- /.container -->
+
+            </nav>
+
+<section id="counter" class="counter-section content-section">
+
+                <div class="container">
+
+                    <div class="row text-center">
+
+                        <div class="col-md-12">
+
+                            <h1 class="white">Learning Programs</h1>
+
+                        </div><!-- /.col-md-12 -->
+                    </div>
+                </div>
+</section>
+<section class="ftco-section testimony-section bg-light">
+      <div class="container">
+        <div class="row text-center">
+          <!-- <div class="col-md-5 heading-section ftco-animate"> -->
+            
+            <p><br>IMPACT aims to cumlate the best of the content for its registrants, grades 7th to 10th,  so that they can address their audience with utmost confidence and voice their perspective with clarity and compellment, captivating the audience, through an interactive learning program, with multitude collaborations with renowned leadership forums and other esteemed organizations.</p>
+            <h2 class="mb-4 pb-3"><strong><br>Our Learning Aims</strong></h2>
+             
+          <!-- </div> -->
+        </div>
+      </div>
+    <div class="container">
+
+                            <div class="row text-center">
+
+                                <div class="col-md-4">
+
+                                    <div class="row services-item text-center wow flipInX" data-wow-offset="10">
+
+                                       
+
+                                        <!-- <h4>Building Confidence</h4> -->
+
+                                        <p>Guiding the registrants to build confidence and curb stage fright.
+                                                                            </p>
+
+                                    </div><!-- /.row -->
+
+                                </div><!-- /.col-md-4 -->
+
+
+
+                                <div class="col-md-4">
+
+                                    <div class="row services-item text-center wow flipInX" data-wow-offset="10">
+
+                                        
+
+                                       <!--  <h4>Public Speaking</h4> -->
+
+                                        <p>Ignite the ability to voice one’s thoughts in our members, while respecting other people’s opinions.</p>
+
+                                    </div>
+
+                                </div>
+
+                                <div class="col-md-4">
+
+                                    <div class="row services-item text-center wow flipInX" data-wow-offset="10">
+
+                                        
+
+                                       <!--  <h4>Leadership skills</h4> -->
+
+                                        <p>Guide the members on the path of becoming tomorrow’s leaders.</p>
+
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+
+<section id="clients" class="our-clients content-section">
+
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="col-md-4">
+
+                            <img src="img/bc.jpg" class="center-block img-responsive">
+
+                        </div><!-- /.col-md-4 -->
+
+
+
+                        <div class="col-md-8">
+
+                            <div class="products-container">
+
+
+
+                                <div class="col-md-12">
+
+                                    <h2>Building Confidence<br></h2>
+                                    <p>We plan to build confidence amongst our registrants by encouraging them to channel their thoughts verbally, as well as motivate them with the enlightening words offered by external speakers throughout the course.</p>
+                                    <p>We will constantly monitor the registrants and see what aspects of public speaking they lack, thereby allowing us to personally address their issues, leaving zero loopholes. We will try to incorporate talks about feelings, emotions, and life goals so that our registrants are comfortable with sharing their personal thoughts with others as it is one of the initial steps in the nurturing of confidence.</p>
+
+
+                                </div><!-- /.col-md-12 -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-md-8">
+
+                            <div class="products-container">
+
+
+
+                                <div class="col-md-12">
+
+                                    <h2><br>Public Speaking<br></h2>
+                                    <p>Public speaking allows us to form connections, influence decisions, and motivate change. Without communication skills, the ability to progress in the working world and in life, itself, would be nearly impossible.</p>
+                                    <p>We want to taper our registrants as effective communicators, who avoid the barriers that come with communication and make their voice heard for the change they desire and the impact that they wish to leave. By conducting numerous interactive sessions and events, we will lead our registrants to the path of understanding the significance of public speaking and effective communication. We will carve their path to becoming the great leaders of the future, with trivial oration techniques.</p>
+
+
+                                </div><!-- /.col-md-12 -->
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+
+                            <br><br><br><img src="img/ps.png" class="center-block img-responsive">
+
+                        </div><!-- /.col-md-4 -->
+                    </div>
+
+                </div><!-- /.container -->
+
+            <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+
+                           <br> <img src="img/mun2.jpeg" class="center-block img-responsive">
+
+                        </div><!-- /.col-md-4 -->
+
+                        <div class="col-md-8">
+
+                            <div class="products-container">
+
+
+
+                                <div class="col-md-12">
+
+                                    <h2>Moon Training Sessions<br></h2>
+                                    <p>Impact will provide exhaustive training sessions for MUNs, preparing all the registrants to flourish in any sort of MUN, as their understanding on politics, significant debating, lobbying, persuasion and negotiation skills will be polished. MUNs will help students to develop critical leadership skills, research tactics, speech writing, public speaking, and integral problem-solving skills which are implementable in real life as well. Along with that, this will give them an  idea of coordination while working with their fellow team members and learn tolerance.</p>
+
+                                </div><!-- /.col-md-12 -->
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                </div><!-- /.container -->
+
+            </section><!-- /.products -->
+
+
+
+
+
+
+            <!-- Footer -->
+
+            <footer>
+
+                <div class="container">
+
+                    <div class="row text-center">
+
+                        <div class="col-md-12 segment">
+
+                            <a href="#" title="Hallooou - HTML5 Template">
+
+                                <h2>
+
+                                    Impact
+
+                                </h2>
+
+                            </a>
+
+                            <p class="white">In a world like today, your voice needs to be heard and reverberated till it leaves a mark of change.</p>
+
+                        </div><!-- /.col-md-12 -->
+
+                    </div><!-- /.row -->
+
+
+
+                    <div class="row text-center">
+
+                        <div class="col-md-12 social segment">
+
+                            <h4>Stay Connected</h4>
+
+                            <a href="#"><i class="fa fa-facebook fa-3x"></i></a>
+
+                            <a href="#"><i class="fa fa-twitter fa-3x"></i></a>
+
+                            <a href="#"><i class="fa fa-linkedin fa-3x"></i></a>
+
+                        </div><!-- /.col-md-12 -->
+
+                    </div><!-- /.row -->
+
+
+
+                </div><!-- /.container -->
+
+
+
+                <div class="copynote">
+
+                    <div class="container">
+
+                        <div class="row">
+
+                            <div class="col-md-12 text-center">
+
+                                &copy; 2020. Impact. All rights reserved.
+
+                            </div><!-- /.col-md-12 -->
+
+
+
+                        </div><!-- /.row -->
+
+                    </div><!-- /.container -->
+
+                </div><!-- /.copynote -->
+
+
+
+                <div class="nav pull-right scroll-top">
+
+                    <a href="#home" title="Scroll to top"><i class="fa fa-angle-up"></i></a>
+
+                </div>
+
+
+
+            </footer>
+
+
+
+
+
+    <!-- jQuery -->
+
+    <script src="assets/js/jquery.min.js"></script>
+
+
+
+    <!-- Bootstrap Core JavaScript -->
+
+    <script src="assets/js/bootstrap.min.js"></script>
+
+
+
+    <!-- Plugin JavaScript -->
+
+    <script src="assets/js/plugins/wow.min.js"></script>
+
+    <script src="assets/js/plugins/owl.carousel.min.js"></script>
+
+    <script src="assets/js/plugins/jquery.parallax-1.1.3.js"></script>
+
+    <script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+
+    <script src="assets/js/plugins/jquery.mb.YTPlayer.min.js"></script>
+
+    <script src="assets/js/plugins/jquery.countTo.js"></script>
+
+    <script src="assets/js/plugins/jquery.inview.min.js"></script>
+
+    <script src="assets/js/plugins/pace.min.js"></script>
+
+    <script src="assets/js/plugins/jquery.easing.min.js"></script>
+
+    <script src="assets/js/plugins/jquery.validate.min.js"></script>
+
+    <script src="assets/js/plugins/additional-methods.min.js"></script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBoVKfEihX__NdMwdDysA6Vve6PE9Ierj4"></script>
+
+
+
+    <!-- Custom JavaScript -->
+
+    <script src="assets/js/hallooou.js"></script>
+
+</body>
+
+
+
+</html>
